ab52f2cfb92489ce6a12b17c6db3039ce37fe7d3
Santhoshvoja63 <147876790+Santhoshvoja63@users.noreply.github.com>
2024-01-25 18:01:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('node:http');
-const hostname = '12.0.0.1';
+const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
